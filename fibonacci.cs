efc8441bdbf4657ca389c20eccd9351f129ab297
Kelauni22 <Kelauni.Cook@gmail.com>
2016-07-24 18:58:53 -0400

    wrote more complex print line - finished

--- fibonacci.cs
+++ fibonacci.cs
@@ -1,21 +1,19 @@
-using System;
-class MainClass{
-	public static void {
+public static void {
 
-		int[] fib_array = new int[40]
-		fib_array[0] = 1;
-		fib_array[1] = 1;
+	int[] fib_array = new int[40]
+	fib_array[0] = 1;
+	fib_array[1] = 1;
 
 
-		for(int i =2; i < 40; i++){
-			
-			fib_array[i] = fib_array[i-2] + fib_array[i-1];
-			
-			
-		}
-
-		console.writeline(fib_array);
-
+	for(int i =2; i < 40; i++){
+		
+		fib_array[i] = fib_array[i-2] + fib_array[i-1];
+		
+		
 	}
+	
+	fib_array.ToList().ForEach(Console.WriteLine(i.toString()));
+	
 
 }
+
